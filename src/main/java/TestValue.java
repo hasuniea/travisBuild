c37f4af08df4e483cca4d123928bcc2b8b5e31de
Hasunie <hasunie@wso2.com>
2019-06-27 12:37:06 +0530

    adding source

---
+++ src/main/java/TestValue.java
@@ -0,0 +1,14 @@
+public class TestValue {
+    public static void main(String[] args) {
+        String value= "test build";
+        String obj = new String();
+        getName();
+
+
+    }
+
+    public static String getName() {
+        String value = "generate name";
+        return value;
+    }
+}
